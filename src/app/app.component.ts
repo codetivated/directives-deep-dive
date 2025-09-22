0566936b4fac1e828585cdd4be15ca620084834a
codetivated <melisa@heying.us>
2025-09-22 16:38:51 -0500

    build an auth custom structural directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,19 @@ import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
 import { inject } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { AuthDirective } from './auth/auth.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent],
+  imports: [
+    AuthComponent,
+    LearningResourcesComponent,
+    AuthDirective,
+    AuthDirective,
+  ],
 })
 export class AppComponent {
   private authService = inject(AuthService);
